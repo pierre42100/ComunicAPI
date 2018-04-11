dcdde1e84b70b75e3f41134176a1ebb70b845bc2
Pierre <pierre@hubert.fr>
2018-04-11 11:19:48 +0200

    Create new account model.

---
+++ classes/models/NewAccount.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * New account model
+ * 
+ * @author Pierre HUBERT
+ */
+
+class NewAccount {
+
+	/**
+	 * The first name of the user
+	 */
+	public $firstName;
+
+	/**
+	 * The last name of the user
+	 */
+	public $lastName;
+
+	/**
+	 * The email address of the user
+	 */
+	public $emailAddress;
+
+	/**
+	 * The password of the user (NOT ENCRYPTED)
+	 */
+	public $password;
+
+}
