a6021aeffce51c3a799ea5a1cb7165cd78aadd78
Pierre HUBERT <pierre@mail.local>
2018-07-12 14:05:56 +0200

    Created first test

---
+++ tests/classes/models/APIClientTest.php
@@ -0,0 +1,16 @@
+<?php
+
+//Include class
+require_once(__DIR__."/../../../classes/models/APIClient.php");
+
+use PHPUnit\Framework\TestCase;
+
+class APIClientTest extends TestCase {
+
+	public function testConfirmHasTokenAfterSet(){
+		$client = new APIClient();
+		$client->set_token("token");
+		$this->assertEquals(TRUE, $client->has_token());
+	}
+
+}
